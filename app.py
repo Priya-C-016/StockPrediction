44c6ec065a8f9ccfa2785526bd1f630ae87a78ef
Priya Chanchal <145918494+Priya-C-016@users.noreply.github.com>
2025-01-14 22:34:12 +0530

    app.py

--- app.py
+++ app.py
@@ -260,7 +260,7 @@ if st.session_state.current_page == 'home':
     # Main container with background and styling
 
     # Title and Subtitle
-    # st.markdown("<h1 class='title'>📈 StockVision 📉</h1>", unsafe_allow_html=True)
+    # st.markdown("<h1 class='title'>📈 Advanced Stock Price Prediction App 📉</h1>", unsafe_allow_html=True)
     # st.markdown("<p class='subtitle'>Your Professional Tool for Stock Analysis and Market Insights 🚀</p>", unsafe_allow_html=True)
     # Sample data for candlestick chart (you can replace this with real data or use more points)
     data = {
@@ -296,7 +296,7 @@ if st.session_state.current_page == 'home':
     
     # Display the chart in Streamlit
     # st.title("Welcome to Stock Market Dashboard")
-    st.markdown("<h1 class='title'>📈 StockVision 📉</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 class='title'>📈 Advanced Stock Price Prediction App 📉</h1>", unsafe_allow_html=True)
     st.markdown("<p class='subtitle'>Your Professional Tool for Stock Analysis and Market Insights 🚀</p>", unsafe_allow_html=True)
 
     st.plotly_chart(fig)
